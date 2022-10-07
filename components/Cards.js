4e4a8bb01d941f94d9f899bc6198cc4e30166434
Helder Fonseca <helder2391@gmail.com>
2022-10-07 14:34:36 -0100

    conect to mailerlite

--- components/Cards.js
+++ components/Cards.js
@@ -1,25 +1,25 @@
 const Cards = () => {
 
     return (
-        <div className="pt-40 mx-auto">
-            <h3 className="text-3xl font-bold text-center pb-10"> What we offer </h3>
-            <div className="mt-8 flex flex-wrap md:flex-nowrap md:flex-row mx-auto justify-center items-center gap-16 md:w-3/4 w-1/2">
-                <div className="bg-[#FBED04] p-4 rounded-md w-[16rem] border cursor-pointer hover:shadow-xl duration-500 ease-out">
-                    <p className="font-bold text-center mb-2 text-[1.3rem]">A Digital Marketing Plan</p>
-                    <p className="text-justify">A Digital Marketing plan is a proposal that reflects the objectives, mission, vision, principles,
+        <div className="pt-28 mx-auto">
+            <h3 className="text-3xl font-bold text-center pb-10">What we offer</h3>
+            <div className="mt-6 flex md:flex-row flex-col mx-auto justify-center items-center gap-8 w-full">
+                <div className="bg-[#FBED04] p-4 rounded-md w-3/5 md:w-[18rem] h-[20'rem] border cursor-pointer hover:shadow-xl duration-500 ease-out">
+                    <p className="font-bold text-center mb-2 text-lg">A Digital Marketing Plan</p>
+                    <p className="text-justify text-base">A Digital Marketing plan is a proposal that reflects the objectives, mission, vision, principles,
                      and values of the company/institution/organization. 
                     The strategy is outlined in close coordination with your communication and marketing team or with the person responsible for that area.</p>
                 </div>
 
-                <div className="bg-[#FBED04] p-4 rounded-md w-[16rem] border cursor-pointer hover:shadow-xl duration-500 ease-out">
-                    <p className="font-bold text-center mb-2 text-[1.3rem]">Content Producer</p>
-                    <p className="text-justify">A content producer on stand-by will implement the agreed content according to guidelines and a pre-defined action schedule.
+                <div className="bg-[#FBED04] p-4 rounded-md w-3/5 md:w-[18rem] h-[20rem] border cursor-pointer hover:shadow-xl duration-500 ease-out">
+                    <p className="font-bold text-center mb-2 text-lg">Content Producer</p>
+                    <p className="text-base text-justify">A content producer on stand-by will implement the agreed content according to guidelines and a pre-defined action schedule.
                      The above contact person will be the focal point with the company/institution/organization.</p>
                 </div>
 
-                <div className="bg-[#FBED04] p-4 rounded-md w-[16rem] border cursor-pointer hover:shadow-xl duration-500 ease-out">
-                    <p className="font-bold text-center mb-9 text-[1.3rem]">A Designer</p>
-                    <p className="text-justify">A designer is available, responsible for the conception and design of the graphic arts that accompanies various publications.</p>
+                <div className="bg-[#FBED04] p-4 rounded-md w-3/5 md:w-[18rem] h-[20rem] border cursor-pointer hover:shadow-xl duration-500 ease-out">
+                    <p className="font-bold text-center mb-2 text-lg">A Designer</p>
+                    <p className="text-base text-justify">A designer is available, responsible for the conception and design of the graphic arts that accompanies various publications.</p>
                 </div>
             </div>
         </div>
--- components/Top.js
+++ components/Top.js
@@ -6,23 +6,21 @@ const Top = () => {
 
     return (
         <div className="mx-auto max-w-6xl">
-            <div className="flex md:flex-row flex-col-reverse gap-16 md:mt-20">
-                <div className="md:w-1/2 w-full">
+            <div className="flex md:flex-row flex-col-reverse gap-16">
+                <div className="md:w-1/2 w-full md:mt-24">
                     <h1 className="text-5xl font-bold md:w-[20rem] ">Social Network Management</h1> 
-                    <p className="mt-4 mb-4 w-96 text-3xl font-thin">
+                    <p className="mt-4 mb-4 md:w-96 text-3xl font-thin">
                        Choose your best plan for your company with the <span className="font-bold italic"> best discount.</span>. 
                     </p>
-                    <p className="mb-4 w-96 text-justify font-medium" >
+                    <p className="mb-4 md:w-96 text-justify font-medium" >
                        Manage your Social Networks by producing content according to a pre-established guideline and schedule to increase and retain your number of online followers. 
                     </p>
-                    <p className="mb-4 w-96 text-justify font-medium" >
+                    <p className="mb-4 md:w-96 text-justify font-medium" >
                        Inform and educate online followers about; your services and products, mission, vision, principles, and values to increase your website traffic 
                     </p>
                 </div>
 
-                <div className="md:w-1/2 w-full">
-                    <Image src={media} width={1090} height={1000} className=""/>
-                </div>
+                <Image src={media} width={640} height={640} alt="People" className="objecct-contain md:w-1/2 w-full"/>
             </div>
         </div>
     )
