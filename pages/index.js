d0d264d44b499ca6fd9442b9077e0c7746266d84
Helder Fonseca <helder2391@gmail.com>
2022-11-16 12:03:15 -0100

    remove mailerlite popup

--- pages/index.js
+++ pages/index.js
@@ -6,26 +6,19 @@ import Header from '../components/Header'
 import Testemunhos from '../components/Testemunhos'
 import Top from '../components/Top'
 import Script from 'next/script'
-import About from '../components/About'
 
 export default function Home() {
   return (
-    <div>
-      <Head>
-        <title>Social Media - Landing Page</title>
-        <meta name="description" content="Social Media Landing Page" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className='pb-10'>
-        <div className='pt-8 md:h-screen pb-8 px-10 bg-[#181818]'>
-          <Header></Header>
-          <Top></Top>
+    <div className='pb-10'>
+        <div className='pt-8 md:h-screen px-10 bg-[#181818]'>
+         <Header></Header>
+         <Top></Top>
         </div>
-        <About />
         <Cards></Cards>
         <Testemunhos></Testemunhos>
-      </main>
+        {/*<Lead />*/}
+        {/*<Testimonials />*/}
+
     </div>
   )
 }
